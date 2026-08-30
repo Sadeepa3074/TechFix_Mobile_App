1097d9d810ecd6e966fa8a2bbd093be22949d751
Rivindu Wickramasekara <rivindupw@gmail.com>
2026-08-31 01:31:05 +0530

    Save local build configuration updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    // Retrofit core and converter
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
